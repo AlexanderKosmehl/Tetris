34a3ee3f6c275881e1738d0b0989ef26ac4df895
Alexander Kosmehl <kosmehl@live.de>
2022-07-03 22:19:02 +0200

    Added GameController and Spawner

---
+++ Assets/GameController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    Board m_gameBoard;
+
+    Spawner m_spawner;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_gameBoard = GameObject.FindWithTag("Board").GetComponent<Board>();
+        m_spawner = GameObject.FindWithTag("Spawner").GetComponent<Spawner>();
+
+        if (!m_gameBoard)
+        {
+            Debug.LogWarning("WARNING! There is no game board defined!");
+        }
+
+        if (!m_spawner)
+        {
+            Debug.LogWarning("WARNING! There is no spawner defined!");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Spawner.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public Shape[] m_allShapes;
+
+    Shape GetRandomShape()
+    {
+        int i = Random.Range(0, m_allShapes.Length);
+        if (!m_allShapes[i])
+        {
+            Debug.Log("WARNING! Invalid shape!");
+            return null;
+        }
+
+        return m_allShapes[i];
+    }
+
+    public Shape SpawnShape()
+    {
+        Shape shape = null;
+        shape = Instantiate(GetRandomShape(), transform.position, Quaternion.identity) as Shape;
+
+        if (!shape)
+        {
+            Debug.LogWarning("WARNING! Invalid shape in spawner!");
+            return null;
+        }
+
+        return shape;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
