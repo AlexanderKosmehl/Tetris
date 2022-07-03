583f607e88f3dd53ac7c3ddd26083d1a81fd65ac
Alexander Kosmehl <kosmehl@live.de>
2022-07-03 21:52:19 +0200

    Implemented Movement Methods

--- Assets/Scripts/Core/Shape.cs
+++ Assets/Scripts/Core/Shape.cs
@@ -4,6 +4,48 @@ using UnityEngine;
 
 public class Shape : MonoBehaviour
 {
+
+    public bool m_canRotate = true;
+
+    void Move(Vector3 moveDirection)
+    { 
+        transform.position += moveDirection;
+    }
+
+    public void MoveLeft()
+    {
+        Move(new Vector3(-1, 0, 0));
+    }
+
+    public void MoveRight()
+    {
+        Move(new Vector3(1, 0, 0));
+    }
+
+    public void MoveDown()
+    {
+        Move(new Vector3(0, -1, 0));
+    }
+
+    public void MoveUp()
+    {
+        Move(new Vector3(0, 1, 0));
+    }
+
+    public void RotateRight()
+    {
+        if (!m_canRotate) return;
+
+        transform.Rotate(0, 0, -90);
+    }
+
+    public void RotateLeft()
+    {
+        if (!m_canRotate) return;
+
+        transform.Rotate(0, 0, 90);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +55,6 @@ public class Shape : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
